d688b3ae0fc3608ed967aa37caa9bbee1f5c9fd6
lee-ho-yun <wjsghk1588@naver.com>
2021-01-18 18:41:47 +0900

    finish

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # movie_app
+유트브 노마더 코더 <br />
+https://nomadcoders.co/<br />
+참고: https://yts.mx/api#list_movies<br />
+https://github.com/serranoarevalo/yts-proxy<br />
--- src/App.js
+++ src/App.js
@@ -1,48 +1,20 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+import { BrowserRouter, Route } from 'react-router-dom'
+import About from './router/About'
+import Home from './router/Home'
+import Navigation from './components/Navigation'
+import Detail from './router/Detail'
+import "./App.css";
 
-class App extends React.Component {
-  // 제일 먼저 호출되는곳
-  constructor(props) {
-    super(props);
-    console.log("hello");
-  }
-  state = {
-    count : 0,
-  };
-  add = () => {
-    this.setState(
-      // 밑에 있는 것보다 더좋은 방법이다.
-      current => ({count: current.count + 1})
-    );
-  };
-  minus = () => {
-    this.setState({
-      count: this.state.count - 1
-    });
-  };
-  // render을 호출후 호출됨
-  componentDidMount() {
-    console.log('component rendered')
-  }
-  // 지금있는 컴포넌트에 변화가 생기면 호출된다.
-  componentDidUpdate() {
-    console.log('I just updated')
-  }
-  // 지금 있는 컴포넌트를 벗어날때 호출된다.
-  componentWillUnmount() {
-    console.log('Goodbye, cruel world')
-  }
-  render() {
-    console.log("I'm rendering");
-    return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>minus</button>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <BrowserRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={ Home } />
+      <Route path="/about" component={ About } />
+      <Route path="/movie/:id" component={ Detail } />
+    </BrowserRouter>
+  );
 }
 
 export default App;
